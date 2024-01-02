7a1603d41a5e592f2da146d1395ff98476030c59
perriex <parna80as@gmail.com>
2024-01-02 16:07:59 +0330

    FEAT: setup express and routes

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const connectToDatabase = require("./database/mongodb");
+
+const postController = require("./controllers/post");
+const authorController = require("./controllers/author");
+const tagController = require("./controllers/tag");
+
+//App Config
+const app = express();
+const port = process.env.PORT || 8085;
+const corsOptions = {
+  origin: [
+    "http://localhost:8080",
+    "http://localhost:3000",
+    "http://admin.bamis.ir",
+    "https://bartarha.net",
+    "https://app.bamis.ir",
+    "http://stageadmin.bamis.ir",
+  ],
+};
+
+//Middleware
+app.use(bodyParser.json());
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//DB Config
+connectToDatabase();
+
+//API Endpoints
+app.use("/post", postController);
+app.use("/author", authorController);
+app.use("/tag", tagController);
+app.get("/", (req, res) => res.send("Hi there!"));
+
+//Listener
+app.listen(port, () => {
+  console.log(`Listening on localhost: ${port}`);
+});
